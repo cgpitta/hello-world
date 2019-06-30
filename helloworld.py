0a1e3d60f4f34093101c4a3e7603e5af9057e01d
Carolyn <52383366+cgpitta@users.noreply.github.com>
2019-06-30 15:47:28 -0400

    Hello world i
    added file that displays 'Hello World' using Python

---
+++ helloworld.py
@@ -0,0 +1,20 @@
+#hello world program 
+
+#function that displays the contents of the parameter obj
+def display(obj):
+
+	print(obj)
+
+
+#create main function
+
+def main():
+	test = 'Hello World'
+
+	display(test)
+
+
+
+#call main function
+
+main()
